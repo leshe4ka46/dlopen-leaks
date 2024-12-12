2cd4f6eeae96dd784194adb2e5cad21f57198888
leshe4ka46 <alex9102naid1@ya.ru>
2024-12-12 10:53:18 +0300

    add pauses

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
+#include <cstdio>
 #include <dlfcn.h>
 
-int main(){
-    void* plug = dlopen("build/libplugin.so", RTLD_LAZY | RTLD_LOCAL);
+int main() {
+    getchar();
+    void *plug = dlopen("build/libplugin.so", RTLD_LAZY | RTLD_LOCAL);
+    getchar();
     dlclose(plug);
+    getchar();
 }
